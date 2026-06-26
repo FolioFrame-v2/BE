657525831db89e9d0543fced52c736a2d81f3525
KoEunB <e_un_04@naver.com>
2026-06-26 13:35:02 +0900

    [Chore] 프로젝트 초기 세팅

---
+++ src/main/java/com/folioframe/FolioframeApplication.java
@@ -0,0 +1,13 @@
+package com.folioframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolioframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolioframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folioframe/FolioframeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folioframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolioframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
