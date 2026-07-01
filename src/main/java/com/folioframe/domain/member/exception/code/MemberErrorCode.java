a833eb8e93e08b7b06cbd2c4517e3543521720fd
EunBi Ko <e_un_04@naver.com>
2026-07-01 10:24:03 +0900

    [Feat] Member 레포지토리 및 예외 코드 추가

---
+++ src/main/java/com/folioframe/domain/member/exception/code/MemberErrorCode.java
@@ -0,0 +1,17 @@
+package com.folioframe.domain.member.exception.code;
+
+import com.folioframe.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorCode implements BaseErrorCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404_1", "회원을 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
